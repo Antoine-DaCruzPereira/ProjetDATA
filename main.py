686256f8a5a4483532b816d58e08a7ad0ab50fff
JungleDarius11 <soen.souprayen@edu.esiee.fr>
2025-10-27 13:10:52 +0000

    Ajout de la creation des graphes dans le main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from src.utils.Download_CSV import download_velib_csv
 from src.utils.CleanData_CSV import clean_velib_csv
 from src.utils.Create_DataBase import create_velib_database
+from src.utils.Histogramme import create_histograms 
 
 if __name__ == "__main__":
     print("\n=== 1. Téléchargement du CSV Vélib ===")
@@ -17,3 +18,10 @@ if __name__ == "__main__":
     create_velib_database()
 
     print("\n=== Pipeline de données terminé avec succès ! ===\n")
+
+    print("\n=== Génération des histogrammes ===")
+    print("----------------------------------------")
+    create_histograms()
+
+    print("\n=== Génération des histogrammes terminée ! ===\n")
+    
